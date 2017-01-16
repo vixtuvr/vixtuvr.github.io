3ed663d2fa2470dba58d3454328bf6898a160d44
vixtuvr <vixtuvr@gmail.com>
2017-01-16 18:10:00 +0100

    Update update-raycaster.js

--- image360demo/components/update-raycaster.js
+++ image360demo/components/update-raycaster.js
@@ -3,6 +3,7 @@ AFRAME.registerComponent('update-raycaster', {
   schema: {type: 'selector'},
 
   init: function () {
+    console.log("Entering init function in update-raycaster.js");
     var raycasterEl = this.data;
     raycasterEl.components.raycaster.refreshObjects();
   }
